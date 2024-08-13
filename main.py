7538849153668a52067487f0eb3358655070af92
Tomasskuta <tomasskuta@seznam.cz>
2024-08-13 13:40:23 +0200

    start

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+projekt_3.py: třetí projekt do Engeto Online Python Akademie
+author: Tomáš Škuta
+email: tomasskuta@seznam.cz
+discord: smajlikskutik
+"""
+
+def main():
+    print("test")
+
+if __name__ == "__main__":
+    main()

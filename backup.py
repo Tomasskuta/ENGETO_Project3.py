9fa2a9c657542ad3e79e80566bef80b468cf1bb9
Tomasskuta <tomasskuta@seznam.cz>
2024-09-11 10:45:18 +0200

    now having every data, now time for csv

--- backup.py
+++ backup.py
@@ -12,34 +12,44 @@ from pprint import pprint
 def main():
     url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7103"
     vystupni_soubor = "test.csv"
+    get_main_data(url)
+
+def get_main_data(url):    
     #get data from url
     odp_serveru = requests.get(url) #stahl sem cely html kod stranky z odkazu
     #parsovani
     soup = BeautifulSoup(odp_serveru.text, 'html.parser') # promenna + typ parseru
     #najdu vsechny tabulky
-    table_tag = soup.find("div", {"id": "core"}) # kdyz tu dam all at najdu vsechny ty tabulky tak pak nejede to niz ?????????????????????????????????????????????
-    #print(table_tag)
+    table_tag = soup.find("div", {"id": "core"}) 
+
     vsechny_tr = table_tag.find_all("tr") 
-    #print(vsechny_tr[2])
 
     vysledky = []
 
-    for tr in vsechny_tr[2:-2]:
-        td_na_radku = tr.find_all("td")
-        #print(td_na_radku[0].get_text())
-        #print(td_na_radku[1].get_text())
-        #link_td = td_na_radku[2].find("a")
-        data = get_first_batch_of_data(td_na_radku)
-        #if link_td and link_td.has_attr('href'):
-            # Get the 'href' attribute (the link)
-            #link = link_td['href']
-            #print("https://volby.cz/pls/ps2017nss/"+link)
+    for tr in vsechny_tr[2:-2]: #ignoruju prvni 2 a posledni 2 radky at mi to nedela binec
+        td_na_radku = tr.find_all("td") #na radsich najdu vsechny bunky
+        data = combine_data(td_na_radku)
         vysledky.append(data)
-        
-    #print(td_na_radku)
+    
     pprint(vysledky)
     
-def get_first_batch_of_data(tr_tag: "bs4.element.ResultSet"):
+def get_data_from_link(link):
+    odp_serveru = requests.get(link) 
+
+    soup = BeautifulSoup(odp_serveru.text, 'html.parser') 
+    table_tag = soup.find("table", {"class": "table"})
+    vsechny_tr = table_tag.find_all("tr") 
+
+    for tr in vsechny_tr[2:]:
+        td_na_radku = tr.find_all("td")
+        return {
+            "registered": td_na_radku[3].get_text(),
+            "envelopes": td_na_radku[4].get_text(),
+            "valid": td_na_radku[7].get_text(),
+        }
+
+
+def combine_data(tr_tag: "bs4.element.ResultSet"):
     """
     Z kazdeho radku (tr) vyber urcite bunky (td)[index])
     a zabal je do slovniku
@@ -47,12 +57,22 @@ def get_first_batch_of_data(tr_tag: "bs4.element.ResultSet"):
     if len(tr_tag) >= 3: #osetreni aby program bral radky kde jsou alespon 3 elementy - cislo, jmeno, link
         link_td = tr_tag[2].find("a") #v tretim tagu center hledam tag mezi a a pak z nej bereme href odkaz
         link = "https://volby.cz/pls/ps2017nss/" + link_td['href']
+        data_from_link = get_data_from_link(link)
+
+        if data_from_link is None:
+            data_from_link = {
+                "registered": "N/A",
+                "envelopes": "N/A",
+                "valid": "N/A",
+            }
+    
         return {
             "cislo": tr_tag[0].getText(),
             "jmeno": tr_tag[1].getText(),
-            "link": link,
+            "registered": data_from_link["registered"],  
+            "envelopes": data_from_link["envelopes"],
+            "valid": data_from_link["valid"]
         }
-
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ discord: smajlikskutik
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
+import csv
 
 def main():
     url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=12&xnumnuts=7103"
@@ -15,19 +16,15 @@ def main():
     get_main_data(url)
 
 def get_main_data(url):    
-    #get data from url
-    odp_serveru = requests.get(url) #stahl sem cely html kod stranky z odkazu
-    #parsovani
-    soup = BeautifulSoup(odp_serveru.text, 'html.parser') # promenna + typ parseru
-    #najdu vsechny tabulky
-    table_tag = soup.find("div", {"id": "core"}) 
+    odp_serveru = requests.get(url) 
+    soup = BeautifulSoup(odp_serveru.text, 'html.parser') 
 
+    table_tag = soup.find("div", {"id": "core"}) 
     vsechny_tr = table_tag.find_all("tr") 
 
     vysledky = []
-
-    for tr in vsechny_tr[2:-2]: #ignoruju prvni 2 a posledni 2 radky at mi to nedela binec
-        td_na_radku = tr.find_all("td") #na radsich najdu vsechny bunky
+    for tr in vsechny_tr[2:-2]: 
+        td_na_radku = tr.find_all("td") 
         data = combine_data(td_na_radku)
         vysledky.append(data)
     
@@ -35,27 +32,44 @@ def get_main_data(url):
     
 def get_data_from_link(link):
     odp_serveru = requests.get(link) 
-
     soup = BeautifulSoup(odp_serveru.text, 'html.parser') 
-    table_tag = soup.find("table", {"class": "table"})
-    vsechny_tr = table_tag.find_all("tr") 
 
-    for tr in vsechny_tr[2:]:
-        td_na_radku = tr.find_all("td")
-        return {
-            "registered": td_na_radku[3].get_text(),
-            "envelopes": td_na_radku[4].get_text(),
-            "valid": td_na_radku[7].get_text(),
+    head_table_tag = soup.find("table", {"class": "table"})
+    vsechny_tr_head = head_table_tag.find_all("tr") 
+    vysledky_head = {}
+    for tr in vsechny_tr_head[2:]:
+        td_na_radku_head = tr.find_all("td")
+        vysledky_head = {
+            "registered": td_na_radku_head[3].get_text(),
+            "envelopes": td_na_radku_head[4].get_text(),
+            "valid": td_na_radku_head[7].get_text(),
         }
+        break
 
+    table_tag = soup.find("div", {"id": "core"})
+    vsechny_tr = table_tag.find_all("tr")
+    vysledky_parties = []
+    for tr in vsechny_tr[5:-1]:
+        td_na_radku = tr.find_all("td")
+        if len(td_na_radku) > 2:
+            vysledky_party = {
+                "strana": td_na_radku[1].get_text(),
+                "hlasy": td_na_radku[2].get_text(),
+            } 
+            vysledky_parties.append(vysledky_party)   
+
+    return {
+        "head": vysledky_head,
+        "parties": vysledky_parties
+    }
 
 def combine_data(tr_tag: "bs4.element.ResultSet"):
     """
     Z kazdeho radku (tr) vyber urcite bunky (td)[index])
     a zabal je do slovniku
     """
-    if len(tr_tag) >= 3: #osetreni aby program bral radky kde jsou alespon 3 elementy - cislo, jmeno, link
-        link_td = tr_tag[2].find("a") #v tretim tagu center hledam tag mezi a a pak z nej bereme href odkaz
+    if len(tr_tag) >= 3: 
+        link_td = tr_tag[2].find("a") 
         link = "https://volby.cz/pls/ps2017nss/" + link_td['href']
         data_from_link = get_data_from_link(link)
 
@@ -69,9 +83,10 @@ def combine_data(tr_tag: "bs4.element.ResultSet"):
         return {
             "cislo": tr_tag[0].getText(),
             "jmeno": tr_tag[1].getText(),
-            "registered": data_from_link["registered"],  
-            "envelopes": data_from_link["envelopes"],
-            "valid": data_from_link["valid"]
+            "registered": data_from_link["head"].get("registered"),  
+            "envelopes": data_from_link["head"].get("envelopes"),
+            "valid": data_from_link["head"].get("valid"),
+            "parties": data_from_link["parties"]
         }
 
 if __name__ == "__main__":
